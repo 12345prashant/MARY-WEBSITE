20c73db9601371d65664da9275c62391acc362c2
AbhayBhan <abhaybhan197@gmail.com>
2024-03-06 20:31:03 +0530

    Fix : Fixed the contact us form functionality

---
+++ forms/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contactForm");
+  
+    contactForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+  
+      const formData = new FormData(contactForm);
+  
+      const payload = {
+        fromEmail: "nandhakumar1411@gmail.com",
+        fromName: "Mary Mcilvain",
+        fromPhone: "+91 70222 41471",
+        name: formData.get('name'),
+        subject: formData.get('subject'),
+        email: formData.get('email'),
+        phoneNumber: "+91 70222 4147124",
+        message: formData.get('message'),
+      };
+  
+      fetch("https://api.thenotary.app/service-form/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(payload),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then(data => {
+          document.querySelector(".sent-message").style.display = "block";
+          contactForm.reset();
+        })
+        .catch(error => {
+          document.querySelector(".error-msg").innerText = "An error occurred while sending your message. Please try again later.";
+          document.querySelector(".error-msg").style.display = "block";
+        });
+    });
+  });
+  
